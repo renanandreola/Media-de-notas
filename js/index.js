9145129bf8ab4e44322b42074a0d48f173c2b044
unknown <renanandreolla@outlook.com>
2019-07-31 15:28:49 -0300

    Cálculo de média de 3 notas do aluno

--- js/index.js
+++ js/index.js
@@ -1,5 +1,14 @@
-var a = parseInt(prompt("Digite a primeira nota:"));
-var b = parseInt(prompt("Digite a segunda nota:"));
-var c = parseInt(prompt("Digite a terceira nota:"));
+var a = parseFloat(prompt("Digite a primeira nota:"));
+var b = parseFloat(prompt("Digite a segunda nota:"));
+var c = parseFloat(prompt("Digite a terceira nota:"));
+var result = "";
 
-if 
+var media = (a + b +c) / 3;
+
+if(media >= 6) {
+result = (a + b +c) / 3
+document.body.append("Você foi aprovado, sua média é: " + result);
+}else if(media <= 6) {
+result = (a + b +c) / 3
+document.body.append("Você foi reprovado, sua média é: " + result);
+}
