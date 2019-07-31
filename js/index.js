3e2af0368325608ec972c3b91c59f5a9f887c1e7
Aluno <renanandreolla@outlook.com>
2019-07-31 19:26:19 -0300

    Correções de linhas que não tinham necessidade

--- js/index.js
+++ js/index.js
@@ -6,9 +6,7 @@ var result = "";
 var media = (a + b +c) / 3;
 
 if(media >= 6) {
-result = (a + b +c) / 3
-document.body.append("Você foi aprovado, sua média é: " + result);
+document.body.append("Você foi aprovado, sua média é: " + media);
 }else if(media <= 6) {
-result = (a + b +c) / 3
-document.body.append("Você foi reprovado, sua média é: " + result);
+document.body.append("Você foi reprovado, sua média é: " + media);
 }
